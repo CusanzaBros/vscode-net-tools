d389a4bd5579b2ad4f00b83b5e44fde7fae6ce6b
zcusanza <zach@cusanza.com>
2024-08-31 17:08:39 -0700

    IPv4 Options

--- src/parsers/packet/ipv4Packet.ts
+++ src/parsers/packet/ipv4Packet.ts
@@ -92,6 +92,31 @@ export class IPv4Packet extends GenericPacket {
 		return ret;
 	}
 
+	get options(): IPv4Option[] {
+        const options: IPv4Option[] = [];
+
+        if (this.ihl * 4 <= 20) {
+            return [];
+        }
+
+        let i = 20; 
+        try {
+            while (i < this.ihl * 4) {
+                const option = IPv4Option.create(new DataView(this.packet.buffer, this.packet.byteOffset + i, this.packet.byteLength - i));
+                if (option.length > 0) {
+                    i += option.length;
+                    options.push(option);
+                } else {
+                    i += 1; 
+                }
+            }
+        } catch (e) {
+            
+        }
+
+        return options;
+    }
+
 	get toString() {
 		return `IPv${this.version}, ${this.ihl}, ${this.typeOfService}, ${this.totalLength}, ${this.identification}, ${this.flags}, ${this.fragmentOffset}, ${this.timeToLive}, 0x${this.protocol.toString(16).padStart(2, "0")}, ${this.headerChecksum}, ${this.srcAddress} > ${this.destAddress}, ${this.innerPacket.toString} `;
 	}
@@ -123,7 +148,171 @@ export class IPv4Packet extends GenericPacket {
 		arr.push(`Header Checksum: (${this.headerChecksum})`);
 		arr.push(`Source Address: (${this.srcAddress})`);
 		arr.push(`Destination Address: (${this.destAddress})`);
+		if(this.options.length > 0) {
+			const optArr: Array<any> = [];
+			optArr.push(`Options`);
+			this.options.forEach(item => {
+				optArr.push(item.toString);
+			});
+			arr.push(optArr);
+		}
 
 		return [arr, this.innerPacket.getProperties];
 	}
 }
+
+class IPv4Option {
+    optionData: DataView;
+    length: number;
+    code: number;
+
+    constructor(dv: DataView, offset: number, length: number, code: number) {
+        this.optionData = new DataView(dv.buffer, offset, length);
+        this.length = length;
+        this.code = code;
+    }
+
+    static create(dv: DataView): IPv4Option {
+		const type = dv.getUint8(0);
+		switch (type) {
+			case 0: // End of Option List
+				return new IPv4Option(dv, 0, 0, 0);
+			case 1:
+				return new IPv4OptionNOP(dv, 0, 1, 1);
+			case 2:
+				return new IPv4OptionSecurity(dv, dv.byteOffset + 2, dv.getUint8(1), 2);
+			case 3:
+				return new IPv4OptionLooseSourceRouting(dv, dv.byteOffset + 2, dv.getUint8(1), 3);
+			case 7:
+				return new IPv4OptionRecordRoute(dv, dv.byteOffset + 2, dv.getUint8(1), 7);
+			case 8:
+				return new IPv4OptionStreamID(dv, dv.byteOffset + 2, dv.getUint8(1), 8);
+			case 9:
+				return new IPv4OptionStrictSourceRouting(dv, dv.byteOffset + 2, dv.getUint8(1), 9);
+			case 68:
+				return new IPv4OptionTimestamp(dv, dv.byteOffset + 2, dv.getUint8(1), 68);
+			default:
+				return new IPv4Option(dv, dv.byteOffset + 2, dv.getUint8(1), type);
+		}
+	}
+	
+
+    get toString(): string {
+        return `Option: ${this.code}, Length: ${this.length}`;
+    }
+}
+
+class IPv4OptionNOP extends IPv4Option {
+    constructor(dv: DataView, offset: number, length: number, code: number) {
+        super(dv, offset, length, code);
+    }
+
+    get toString(): string {
+        return `No-Operation (${this.code})`;
+    }
+}
+
+class IPv4OptionRecordRoute extends IPv4Option {
+    constructor(dv: DataView, offset: number, length: number, code: number) {
+        super(dv, offset, length, code);
+    }
+
+    get routeAddresses(): number[] {
+        const addresses = [];
+        for (let i = 0; i < this.length - 2; i += 4) {
+            addresses.push(this.optionData.getUint32(i));
+        }
+        return addresses;
+    }
+
+    get toString(): string {
+        return `Record Route Option (${this.code}) - Addresses: ${this.routeAddresses.join(', ')}`;
+    }
+}
+
+class IPv4OptionSecurity extends IPv4Option {
+    constructor(dv: DataView, offset: number, length: number, code: number) {
+        super(dv, offset, length, code);
+    }
+
+    get securityData(): Uint8Array {
+        return new Uint8Array(this.optionData.buffer.slice(this.optionData.byteOffset, this.optionData.byteOffset + this.length));
+    }
+
+    get toString(): string {
+        return `Security Option (${this.code}) - Data: ${Array.from(this.securityData).join(', ')}`;
+    }
+}
+
+class IPv4OptionLooseSourceRouting extends IPv4Option {
+    constructor(dv: DataView, offset: number, length: number, code: number) {
+        super(dv, offset, length, code);
+    }
+
+    get routeAddresses(): number[] {
+        const addresses = [];
+        for (let i = 0; i < this.length - 2; i += 4) {
+            addresses.push(this.optionData.getUint32(i));
+        }
+        return addresses;
+    }
+
+    get toString(): string {
+        return `Loose Source Routing Option (${this.code}) - Addresses: ${this.routeAddresses.join(', ')}`;
+    }
+}
+
+class IPv4OptionStrictSourceRouting extends IPv4Option {
+    constructor(dv: DataView, offset: number, length: number, code: number) {
+        super(dv, offset, length, code);
+    }
+
+    get routeAddresses(): number[] {
+        const addresses = [];
+        for (let i = 0; i < this.length - 2; i += 4) {
+            addresses.push(this.optionData.getUint32(i));
+        }
+        return addresses;
+    }
+
+    get toString(): string {
+        return `Strict Source Routing Option (${this.code}) - Addresses: ${this.routeAddresses.join(', ')}`;
+    }
+}
+
+class IPv4OptionStreamID extends IPv4Option {
+    constructor(dv: DataView, offset: number, length: number, code: number) {
+        super(dv, offset, length, code);
+    }
+
+    get streamID(): number {
+        return this.optionData.getUint16(0);
+    }
+
+    get toString(): string {
+        return `Stream ID Option (${this.code}) - StreamID: ${this.streamID}`;
+    }
+}
+
+class IPv4OptionTimestamp extends IPv4Option {
+    constructor(dv: DataView, offset: number, length: number, code: number) {
+        super(dv, offset, length, code);
+    }
+
+    get timestamps(): { address: number, timestamp: number }[] {
+        const records = [];
+        for (let i = 0; i < this.length - 4; i += 8) {
+            records.push({
+                address: this.optionData.getUint32(i),
+                timestamp: this.optionData.getUint32(i + 4)
+            });
+        }
+        return records;
+    }
+
+    get toString(): string {
+        return `Internet Timestamp Option (${this.code}) - Records: ${this.timestamps.map(rec => `{Address: ${rec.address}, Timestamp: ${rec.timestamp}}`).join(', ')}`;
+    }
+}
+
+
